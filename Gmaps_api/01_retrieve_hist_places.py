76047262a8fd1934ca996757ea605507bff6df46
Raphael B <raph.bub@gmail.com>
2024-05-09 19:33:31 +0200

    Récupérations infos des lieux

--- Gmaps_api/01_retrieve_hist_places.py
+++ Gmaps_api/01_retrieve_hist_places.py
@@ -1,16 +1,74 @@
 # Nécessite python 3.7(.16)
 from serpapi import GoogleSearch
+import requests
 
-# Effectuer une recherche sur un lieu précis
-params = {
+# Définir quelques lieux
+places = {
+    "Tour Eiffel": "Tour Eiffel",
+    "Colisee": "Colosseum",
+    "Acropole": "Acropolis",
+    "Masjid al-Haram": "Masjid al-Haram",
+    "Fontaine de Trevi": "Trevi fountain",
+    "Porte de l'Inde": "Gateway Of India",
+    "Place de la Constitution": "Plaza de la Constitución, Mexico",
+    "Musée du Louvre": "Louvre Museum",
+    "Taj Mahal": "Taj Mahal"
+}
+
+# Définir les paramètres de recherche des lieux
+gmaps_place_params = {
   "engine": "google_maps",
   "type": "search",
-  "q": "Coliseum", # query
-  "ll": "@40.7455096,-74.0083012,14z", # lat, long, zoom
   "google_domain": "google.com",
   "hl": "en",
   "api_key": "cle_api" # cle api
 }
 
-search = GoogleSearch(params) # Effectuer la recherche
-results = search.get_dict()
+# Initialiser les lieux
+places_info = {}
+
+# Fonction pour récupérer le code iso du pays sur GeoNames 
+def get_country_iso(lat, lng):
+    base_url = "http://api.geonames.org/countryCode"
+    params = {
+        "lat": lat,
+        "lng": lng,
+        "type": "JSON",
+        "username": "" # nom_utilisateur
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    return data.get("countryCode")
+
+# Itérer sur les lieux
+for nom, name in places.items():
+    # Ajouter l'id du lieu aux paramètres
+    gmaps_place_params["q"] = name
+    # Lancer la recherche et extraire les résultats
+    search_place = GoogleSearch(gmaps_place_params)
+    rslts_place = search_place.get_dict()
+
+    # Récupérer le premier résultat
+    rslts_place = rslts_place['local_results'][0]
+
+    # Préparer l'array de reviews
+    place_info = {}
+    # Récupérer les informations utiles
+    place_info['name'] = rslts_place['title']
+    place_info['place_id'] = rslts_place['place_id']
+    place_info['description'] = rslts_place['description']
+    place_info['latitude'] = round(rslts_place['gps_coordinates']['latitude'], 5)
+    place_info['longitude'] = round(rslts_place['gps_coordinates']['longitude'], 5)
+    place_info['rating'] = rslts_place['rating']
+    place_info['num_reviews'] = rslts_place['reviews']
+    place_info['adresse']= rslts_place['address']
+
+    # Récupérer le code du pays
+    place_info['country_iso'] = get_country_iso(place_info['latitude'], place_info['longitude'])
+    
+    # Ajouter au résultat final
+    places_info[nom] = place_info
+
+# Exporter les résultats
+with open("places_test.json", "w") as outfile: 
+    json.dump(places_info, outfile)
--- Gmaps_api/readme.md
+++ Gmaps_api/readme.md
@@ -10,4 +10,18 @@ conda activate pws24_py3716
 pip install google-search-results
 ```
 
+#### 00/01 Récupérer les infos générales sur les lieux
+
+Il faudrait réussir à récupérer une liste de lieux à partir de laquelle on pourrait extraire les noms pour récupérer les infos et ensuite les reviews de google maps
+
+Le script 00 est un test avec la liste des sites de l'UNESCO, mais ceux-ci font parfois référence à des sites qui s'étendent sur plusieurs dizaines voire centaines d'hectares et qui n'ont pas de correspondance "discrète" dans GMaps. Le fichier _cultural_places_v1.json_ contient un exemple du résultat.
+
+Dans le script 01, une liste prédéfinie de quelques lieux sert d'exemple pour la récupération des informations avec Serp API. Il faudra décider de la façon dont les étoffer. Actuellement, on récupère le code iso du pays (2 lettres) à l'aide de l'API [GeoNames](https://www.geonames.org/), mais il faudra voir si cela vaut la peine de creuser pour essayer d'extraire la ville également (pas sur l'API).
+
+Le fichier _places_test.json_ contient un aperçu du résultat de l'extraction GMaps + GeoNames.
+
+#### 02 Récupérer les reviews des lieux
+
+*Cette partie est encore en test !*
+
 Dans json_rslt se trouve un pseudo résultat de ce qui pourrait être extrait par l'api (problèmes de requêtes donc pas pu être testé en entier). Mais le principe de un monument qui a un array de reviews semble tenir.
