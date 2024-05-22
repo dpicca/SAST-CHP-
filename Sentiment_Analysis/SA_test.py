db5886eb7fa0432033169d5453f8446dd192c587
Jeremias Kuehne <32126633+Tchetchouille@users.noreply.github.com>
2024-05-22 12:19:03 +0200

    Deleted Sentiment_Analysis directory
    As the sentiment analysis now takes place in a google collab notebook that I will commit shortly.

--- Sentiment_Analysis/SA_test.py
+++
@@ -1,8 +0,0 @@
-from gpt4all import GPT4All
-from transformers import AutoTokenizer, AutoModelForCausalLM
-model_path = 'lzw1008/Emollama-chat-13b'
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto')
-# model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-output = model.generate("The capital of France is ", max_tokens=10)
-print(output)
